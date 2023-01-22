c4eed2c91abf313975c81b6f2a17488775950843
Gábor Librecz <kuglee@gmail.com>
2023-01-22 17:30:25 +0100

    Initial Commit

---
+++ new/newApp.swift
@@ -0,0 +1,17 @@
+//
+//  newApp.swift
+//  new
+//
+//  Created by Librecz Gábor on 2023. 01. 22..
+//
+
+import SwiftUI
+
+@main
+struct newApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
