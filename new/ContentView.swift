07d32ba98fc65b05a0770c4a49e57c60e1dd8127
Gábor Librecz <kuglee@gmail.com>
2023-01-24 17:12:24 +0100

    ios-debug with HotRealoading and Inject

--- new/ContentView.swift
+++ new/ContentView.swift
@@ -1,26 +1,31 @@
-//
-//  ContentView.swift
-//  new
-//
-//  Created by Librecz Gábor on 2023. 01. 22..
-//
-
 import SwiftUI
 
+#if DEBUG
+  @_exported import Inject
+#endif
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundColor(.accentColor)
+      Text("Hello, world!")
     }
+    .padding()
+    .onAppear {
+      print("hello")
+    }
+    .enableInjection()
+  }
+
+  #if DEBUG
+    @ObserveInjection var inject
+  #endif
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
--- new/newApp.swift
+++ new/newApp.swift
@@ -1,17 +1,10 @@
-//
-//  newApp.swift
-//  new
-//
-//  Created by Librecz Gábor on 2023. 01. 22..
-//
-
 import SwiftUI
 
 @main
 struct newApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
